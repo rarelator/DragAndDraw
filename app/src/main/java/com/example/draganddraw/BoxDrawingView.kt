4e3862c9e9171527045e0d0d0298b4f102ece3e4
Ross Anthony R Relator <ross.relator@uconn.edu>
2023-11-22 13:36:59 -0500

    chap 24 video

--- app/src/main/java/com/example/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/example/draganddraw/BoxDrawingView.kt
@@ -1,10 +1,72 @@
 package com.example.draganddraw
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
-data class BoxDrawingView(
-    val context: Context,
-    val attrs: AttributeSet? = null
-): View(context, attrs)
+private const val TAG = "BoxDrawingView"
+class BoxDrawingView(
+    context: Context,
+    attrs: AttributeSet? = null
+): View(context, attrs) {
+
+    private var currentBox: Box? = null
+    private val boxes = mutableListOf<Box>()
+    private val boxPaint = Paint().apply{
+        color=0x22ff0000.toInt()
+    }
+    private val backgroundPaint = Paint().apply{
+        color=0xfff8efe0.toInt()
+    }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+//        return super.onTouchEvent(event)
+        val current = PointF(event.x, event.y)
+        var action = ""
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                action = "ACTION_DOWN"
+                currentBox = Box(current).also {
+                    boxes.add(it)
+                }
+            }
+
+            MotionEvent.ACTION_UP -> {
+                action = "ACTION_UP"
+                updateCurrentBox(current)
+                currentBox = null
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                action = "ACTION_MOVE"
+                updateCurrentBox(current)
+            }
+
+            MotionEvent.ACTION_CANCEL -> {
+                action = "ACTION_CANCEl"
+                currentBox = null
+            }
+        }
+        Log.d(TAG, "$action at x=${current.x}, y=${current.y}")
+        return true
+    }
+
+    private fun updateCurrentBox(current: PointF) {
+        currentBox?.let {
+            it.end = current
+            invalidate()
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawPaint(backgroundPaint)
+        boxes.forEach{box ->
+            canvas.drawRect(box.left, box.top, box.right, box.bottom, boxPaint)
+        }
+    }
+}
